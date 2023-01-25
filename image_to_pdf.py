e8a4850eb6b0e0456646c73e91b83951ea3b65c1
Gabriel de Oliveira Peres <gabriel.peres@franq.com.br>
2023-01-25 10:35:32 -0300

    Projeto final com executável funcionando

--- image_to_pdf.py
+++ image_to_pdf.py
@@ -1,13 +1,20 @@
 from PIL import Image
+import win32com.client as win32
 
+#### Transforma o formato PNG em PDF #### 
 def imagem_png():
-    image_1 = Image.open(r"\ConvertImagemParaPDF\imagem1.jpg")
-    img_1 = image_1.convert('RBG')
-    img_1.save(r'\ ConvertImagemParaPDF\ imagem1.pdf')
-    
-def imagem_jpg():
-    image_1 = Image.open(r'C:\Usuários\Gabriel Peres\Documentos\Estudos\python\ConvertImagemParaPDF\imagem1.jpg')
-    img_1 = image_1.convert('RBG')
-    img_1.save(r'\ ConvertImagemParaPDF\ imagem1.pdf')
-    
+    image_1 = Image.open(r"imagem1.png")
+    img_1 = image_1.convert('RGB')
+    img_1.save(r'imagem1.pdf')
+
+# #### Transforma o formato JPG em PDF #### 
+# def imagem_jpg():
+#     image_1 = Image.open(r'imagem1.jpg')
+#     img_1 = image_1.convert('RGB')
+#     img_1.save(r'imagem1.pdf')
+
+#### Chama a função PNG para PDF #### 
 imagem_png()
+
+#### Chama a função JPG para PDF #### 
+# imagem_jpg()
