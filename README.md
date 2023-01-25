3a4fcadaed82a40e65b237a21897d6a3e6ee6904
Gabriel de Oliveira Peres <gabriel.peres@franq.com.br>
2023-01-25 10:53:06 -0300

    adicionada uma hotfix que faz imagem jpg e png ao mesmo tempo, precisa comentar o codigo se quiser rodar um de cada vez

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Repositório do código que faz a conversão de Imagens para PDFs
 
 #### Passo a passo
     ##### Crie uma pasta e coloque o executável dentro dela
-    ##### Coloque uma imagem no formato PNG e renomeie para "imagem1.png"
+    ##### Coloque uma imagem no formato PNG e JPG renomeie para "imagempng.png" e "imagemjpg.jpg"
     ##### Rode o código e seja feliz :)
--- image_to_pdf.py
+++ image_to_pdf.py
@@ -3,18 +3,18 @@ import win32com.client as win32
 
 #### Transforma o formato PNG em PDF #### 
 def imagem_png():
-    image_1 = Image.open(r"imagem1.png")
+    image_1 = Image.open(r"imagempng.png")
     img_1 = image_1.convert('RGB')
-    img_1.save(r'imagem1.pdf')
+    img_1.save(r'Documentopng_to_pdf.pdf')
 
-# #### Transforma o formato JPG em PDF #### 
-# def imagem_jpg():
-#     image_1 = Image.open(r'imagem1.jpg')
-#     img_1 = image_1.convert('RGB')
-#     img_1.save(r'imagem1.pdf')
+#### Transforma o formato JPG em PDF #### 
+def imagem_jpg():
+    image_1 = Image.open(r'imagemjpg.jpg')
+    img_1 = image_1.convert('RGB')
+    img_1.save(r'Documentojpg_to_pdf.pdf')
 
 #### Chama a função PNG para PDF #### 
 imagem_png()
 
 #### Chama a função JPG para PDF #### 
-# imagem_jpg()
+imagem_jpg()
